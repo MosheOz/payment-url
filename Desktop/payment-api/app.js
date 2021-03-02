c9a457e5d4110fe820ded12924e89eaa7ba601a9
Moshe <moz770@gmail.com>
2021-03-03 01:19:45 +0200

    add implementation

--- Desktop/payment-api/app.js
+++ Desktop/payment-api/app.js
@@ -34,7 +34,7 @@ vars["ErrorRedirectUrl"] =
 // Other Optional vars :
 
 vars["CancelType"] = "2"; // show Cancel button on start ,
-// $vars["CancelUrl"] ="http://www.yoursite.com/OrderCanceld";
+vars["CancelUrl"] ="http://www.google.com/";
 vars["IndicatorUrl"] = "http://www.yoursite.com/NotifyURL"; // Indicator Url \ Notify URL . after use -  http://kb.cardcom.co.il/article/AA-00240/0
 
 vars["ReturnValue"] = "1234"; // Optional , ,recommended , value that will be return and save in CardCom system
@@ -53,27 +53,42 @@ app.get("/", async (req, res) => {
     // article for invoice vars:  http://kb.cardcom.co.il/article/AA-00244/0
     vars["IsCreateInvoice"] = "true";
     // customer info :
-    vars["InvoiceHead.CustName"] = 'Test "customer'; // customer name
+    vars["InvoiceHead.CustName"] = "Moshe"; // customer name
     vars["InvoiceHead.SendByEmail"] = "true"; // will the invoice be send by email to the customer
     vars["InvoiceHead.Language"] = "he"; // he or en only
     vars["InvoiceHead.Email"] = "test@gmail.com";
 
     // products info
 
-    // Line 1
-    vars["InvoiceLines1.Description"] = "itme 1";
-    vars["InvoiceLines1.Price"] = "5";
-    vars["InvoiceLines1.Quantity"] = "2";
+    const arr = [
+      {
+        Description: "itme 1",
+        Price: "5",
+        Quantity: "2",
+      },
+      {
+        Description: "itme 2",
+        Price: "10",
+        Quantity: "1",
+      },
+    ];
+    arr.forEach((data, index) => {
+      vars[`InvoiceLines${index + 1}.Description`] = data.Description;
+      vars[`InvoiceLines${index + 1}.Price`] = data.Price;
+      vars[`InvoiceLines${index + 1}.Quantity`] = data.Quantity;
+      console.log(data, index + 1);
+    });
 
-    // line 2
-    vars["InvoiceLines2.Description"] = "itme 2";
-    vars["InvoiceLines2.Price"] = "10";
-    vars["InvoiceLines2.Quantity"] = "1";
-
+    let InvoiceLines1 = {
+      Description: "itme 1",
+      Price: "5",
+      Quantity: "2",
+    };
     // ********   Sum of all Lines Price*Quantity  must be equals to SumToBill ***** //
   }
 
   const str = querystring.encode(vars);
+  console.log(vars);
 
   try {
     const response = await axios.post(
@@ -85,19 +100,21 @@ app.get("/", async (req, res) => {
 
     if (obj.ResponseCode == "0") {
       if (IsIframe) {
-          console.log(obj.url)
+        console.log(obj.url);
+        // res.redirect(obj.url);
         res.render("iframe", { data: obj.url });
+        return;
       } // redirect
       else {
         res.redirect(obj.url);
       }
     } else {
       console.error(response);
-      res.status(500).json({error: response})
+      res.status(500).json({ error: response });
     }
-} catch (err) {
+  } catch (err) {
     console.error(err);
-    res.status(500).json({error: err})
+    res.status(500).json({ error: err });
   }
 });
 
