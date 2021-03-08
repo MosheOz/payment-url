8ed1ea73732040beceef035fa3c58be9d5ff864e
Moshe Oz <45286909+MosheOz@users.noreply.github.com>
2021-03-09 00:38:38 +0200

    update app and add vars helper

--- app.js
+++ app.js
@@ -3,82 +3,34 @@ const cors = require("cors");
 const querystring = require("querystring");
 const axios = require("axios");
 
+const { defineVarsArr, defineVarInvoice } = require("./vars.helper");
 const app = express();
 const PORT = 3000;
 
-const TerminalNumber = 1000; // Company terminal
-const UserName = "barak9611"; // API User
 const CreateInvoice = true; // to Create Invoice (Need permissions to create invoice )
 const IsIframe = true; // Iframe or Redirect
-const Operation = 4;
+let LOW_PROFILE_CODE = "";
 
 // Create Post Information
 // Account vars
-const vars = [];
-vars["TerminalNumber"] = TerminalNumber;
-vars["UserName"] = UserName;
-vars["APILevel"] = "10"; // req
-vars["codepage"] = "65001"; // unicode
-vars["Operation"] = Operation;
-
-vars["Language"] = "en"; // page languge he- hebrew , en - english , ru , ar
-vars["CoinID"] = "1"; // billing coin , 1- NIS , 2- USD other , article :  http://kb.cardcom.co.il/article/AA-00247/0
-vars["SumToBill"] = "20"; // Sum To Bill
-vars["ProductName"] = "Test Product"; // Product Name , will how if no invoice will be created.
-
-vars["SuccessRedirectUrl"] =
-  "https://secure.cardcom.solutions/DealWasSuccessful.aspx"; // Success Page
-vars["ErrorRedirectUrl"] =
-  "https://secure.cardcom.solutions/DealWasUnSuccessful.aspx?customVar=1234"; // Error Page
-
-// Other Optional vars :
-
-vars["CancelType"] = "2"; // show Cancel button on start ,
-vars["CancelUrl"] ="http://www.google.com/";
-vars["IndicatorUrl"] = "http://www.yoursite.com/NotifyURL"; // Indicator Url \ Notify URL . after use -  http://kb.cardcom.co.il/article/AA-00240/0
-
-vars["ReturnValue"] = "1234"; // Optional , ,recommended , value that will be return and save in CardCom system
-vars["MaxNumOfPayments"] = "12"; // max num of payments to show  to the user
-
-vars["ShowInvoiceHead"] = "true"; //  if show & edit Invoice Details on the page.
-vars["InvoiceHeadOperation"] = "1";
+let vars = [];
 
 app.use(cors());
 app.use(express.json());
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-app.get("/", async (req, res) => {
+app.use("/payment-token", require('./routes').token);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// regular payment - cc charge
+app.post("/payment", async (req, res) => {
+  vars = defineVarsArr();
   if (CreateInvoice) {
-    // article for invoice vars:  http://kb.cardcom.co.il/article/AA-00244/0
-    vars["IsCreateInvoice"] = "true";
-    // customer info :
-    vars["InvoiceHead.CustName"] = "Moshe"; // customer name
-    vars["InvoiceHead.SendByEmail"] = "true"; // will the invoice be send by email to the customer
-    vars["InvoiceHead.Language"] = "he"; // he or en only
-    vars["InvoiceHead.Email"] = "test@gmail.com";
-
-    // products info
-
-    const arr = [
-      {
-        Description: "itme 1",
-        Price: "5",
-        Quantity: "2",
-      },
-      {
-        Description: "itme 2",
-        Price: "10",
-        Quantity: "1",
-      },
-    ];
-    arr.forEach((data, index) => {
-      vars[`InvoiceLines${index + 1}.Description`] = data.Description;
-      vars[`InvoiceLines${index + 1}.Price`] = data.Price;
-      vars[`InvoiceLines${index + 1}.Quantity`] = data.Quantity;
-      console.log(data, index + 1);
-    });
-    // ********   Sum of all Lines Price*Quantity  must be equals to SumToBill ***** //
+    vars = defineVarInvoice();
   }
 
   const str = querystring.encode(vars);
@@ -93,6 +45,8 @@ app.get("/", async (req, res) => {
     const obj = querystring.decode(response.data);
 
     if (obj.ResponseCode == "0") {
+      LOW_PROFILE_CODE = obj.LowProfileCode; // save low profile code from response.
+      // console.log(obj.PayPalUrl);
       if (IsIframe) {
         console.log(obj.url);
         // res.redirect(obj.url);
@@ -111,5 +65,37 @@ app.get("/", async (req, res) => {
     res.status(500).json({ error: err });
   }
 });
+
+// token charge
+
+//success page
+app.get("/success-page", (req, res) => {
+  res.render("success");
+});
+
+// Indicator URL
+app.get("/NotifyURL", async (req, res) => {
+  try {
+    const isDeal = await getDealIndication();
+    if (isDeal.ResponseCode == 0 && isDeal.lowprofilecode) {
+      res.json(isDeal);
+    }
+  } catch (err) {
+    console.error(err);
+    res.json(false);
+  }
+});
+
+async function getDealIndication() {
+  const url = `https://secure.cardcom.solutions/Interface/BillGoldGetLowProfileIndicator.aspx?`;
+  let config = [];
+  config["terminalnumber"] = 1000;
+  config["username"] = "barak9611";
+  config["lowprofilecode"] = "2e4346ba-e521-4d87-95cd-cf160c39dbc9";
+  const str = querystring.encode(config);
+  const response = await axios.get(url + str);
+  const obj = querystring.decode(response.data);
+  return obj;
+}
 
 app.listen(PORT, console.log("App is running on ", PORT));
---
+++ vars.helper.js
@@ -0,0 +1,88 @@
+const TerminalNumber = 1000; // Company terminal
+const UserName = "barak9611"; // API User
+const CreateInvoice = true; // to Create Invoice (Need permissions to create invoice )
+const Operation = 4;
+const vars = [];
+
+module.exports = {
+  defineVarInvoice: () => {
+    // article for invoice vars:  http://kb.cardcom.co.il/article/AA-00244/0
+    vars["IsCreateInvoice"] = "true";
+    // customer info :
+    vars["InvoiceHead.CustName"] = "Moshe"; // customer name
+    vars["InvoiceHead.SendByEmail"] = "true"; // will the invoice be send by email to the customer
+    vars["InvoiceHead.Language"] = "he"; // he or en only
+    vars["InvoiceHead.Email"] = "test@gmail.com";
+
+    // products info
+
+    const arr = [
+      {
+        Description: "Apples",
+        Price: "12.25",
+        Quantity: "2",
+      },
+      {
+        Description: "Orange",
+        Price: "7.3",
+        Quantity: "3",
+      },
+      {
+        Description: "Tomato",
+        Price: "3",
+        Quantity: "5",
+      },
+      {
+        Description: "Cucumber",
+        Price: "6",
+        Quantity: "1",
+      },
+      {
+        Description: "Guava",
+        Price: "4",
+        Quantity: "3",
+      },
+    ];
+    arr.forEach((data, index) => {
+      vars[`InvoiceLines${index + 1}.Description`] = data.Description;
+      vars[`InvoiceLines${index + 1}.Price`] = data.Price;
+      vars[`InvoiceLines${index + 1}.Quantity`] = data.Quantity;
+    });
+
+    return vars;
+    // ********   Sum of all Lines Price*Quantity  must be equals to SumToBill ***** //
+  },
+
+  defineVarsArr: () => {
+    vars["TerminalNumber"] = TerminalNumber;
+    vars["UserName"] = UserName;
+    vars["APILevel"] = "10"; // req
+    vars["codepage"] = "65001"; // unicode
+    vars["Operation"] = Operation;
+
+    vars["Language"] = "en"; // page languge he- hebrew , en - english , ru , ar
+    vars["CoinID"] = "1"; // billing coin , 1- NIS , 2- USD other , article :  http://kb.cardcom.co.il/article/AA-00247/0
+    vars["SumToBill"] = "79.4"; // Sum To Bill
+    vars["ProductName"] = "Your Cart Details"; // Product Name , will how if no invoice will be created.
+
+    vars["SuccessRedirectUrl"] = "http://localhost:3000/success-page"; // Success Page
+    vars["ErrorRedirectUrl"] =
+      "https://secure.cardcom.solutions/DealWasUnSuccessful.aspx?customVar=1234"; // Error Page
+
+    // Other Optional vars :
+
+    vars["CancelType"] = "2"; // show Cancel button on start ,
+    vars["CancelUrl"] = "https://payment-cardcom.herokuapp.com/";
+    vars["IndicatorUrl"] = "http://localhost:3000/NotifyURL"; // Indicator Url \ Notify URL . after use -  http://kb.cardcom.co.il/article/AA-00240/0
+
+    vars["ReturnValue"] = "1234"; // Optional , ,recommended , value that will be return and save in CardCom system
+    vars["MaxNumOfPayments"] = "12"; // max num of payments to show  to the user
+
+    vars["ShowInvoiceHead"] = "true"; //  if show & edit Invoice Details on the page.
+    vars["InvoiceHeadOperation"] = "1";
+
+    return vars;
+  }
+};
+
+
