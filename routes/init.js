0b3d4c5490d48bee78d0a51608dcd28fef7e6c22
Moshe Oz <45286909+MosheOz@users.noreply.github.com>
2021-03-09 15:25:30 +0200

    Update init.js

--- routes/init.js
+++ routes/init.js
@@ -6,5 +6,10 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.render("index");
 })
-
+router.get('/payment', (req, res) => {
+  res.json({test: "test"});
+})
+router.post('/payment', (req, res) => {
+  res.json({test: "test"});
+})
 module.exports = router;
