9d9b811c650bf199d77773ead0f4e7a87853b257
Moshe Oz <45286909+MosheOz@users.noreply.github.com>
2021-03-09 02:14:10 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -3,70 +3,61 @@ const cors = require("cors");
 const querystring = require("querystring");
 const axios = require("axios");
 
-//const { defineVarsArr, defineVarInvoice } = require("./vars.helper");
+// const { defineVarsArr, defineVarInvoice } = require("./vars.helper");
 const app = express();
 const PORT = 3000;
-
-const CreateInvoice = true; // to Create Invoice (Need permissions to create invoice )
-const IsIframe = true; // Iframe or Redirect
-let LOW_PROFILE_CODE = "";
-
-// Create Post Information
-// Account vars
-let vars = [];
 
 app.use(cors());
 app.use(express.json());
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-
+app.use("/payment-token", require("./routes").token);
+app.use("/payment", require("./routes").payment);
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.use("/payment-token", require('./routes').token);
-
 // regular payment - cc charge
-app.post("/payment", async (req, res) => {
-  //vars = defineVarsArr();
-  if (CreateInvoice) {
-    //vars = defineVarInvoice();
-  }
+// app.post("/payment", async (req, res) => {
+//   vars = defineVarsArr();
+//   if (CreateInvoice) {
+//     vars = defineVarInvoice();
+//   }
 
-  const str = querystring.encode(vars);
-  console.log(vars);
+//   const str = querystring.encode(vars);
+//   console.log(vars);
 
-  try {
-    const response = await axios.post(
-      "https://secure.cardcom.solutions/Interface/LowProfile.aspx",
-      str
-    );
+//   try {
+//     const response = await axios.post(
+//       "https://secure.cardcom.solutions/Interface/LowProfile.aspx",
+//       str
+//     );
 
-    const obj = querystring.decode(response.data);
+//     const obj = querystring.decode(response.data);
 
-    if (obj.ResponseCode == "0") {
-      LOW_PROFILE_CODE = obj.LowProfileCode; // save low profile code from response.
-      // console.log(obj.PayPalUrl);
-      if (IsIframe) {
-        console.log(obj.url);
-        // res.redirect(obj.url);
-        res.render("iframe", { data: obj.url });
-        return;
-      } // redirect
-      else {
-        res.redirect(obj.url);
-      }
-    } else {
-      console.error(response);
-      res.status(500).json({ error: response });
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: err });
-  }
-});
+//     if (obj.ResponseCode == "0") {
+//       LOW_PROFILE_CODE = obj.LowProfileCode; // save low profile code from response.
+//       // console.log(obj.PayPalUrl);
+//       if (IsIframe) {
+//         console.log(obj.url);
+//         // res.redirect(obj.url);
+//         res.render("iframe", { data: obj.url });
+//         return;
+//       } // redirect
+//       else {
+//         res.redirect(obj.url);
+//       }
+//     } else {
+//       console.error(response);
+//       res.status(500).json({ error: response });
+//     }
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ error: err });
+//   }
+// });
 
 // token charge
 
