0735cf44ab7918f6b9294bc565eb448d58a4876c
Moshe Oz <45286909+MosheOz@users.noreply.github.com>
2021-03-10 15:11:46 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,56 +1,68 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const express = require("express");
 const cors = require("cors");
 const querystring = require("querystring");
 const axios = require("axios");
-const routes = require('./routes')
-// const { defineVarsArr, defineVarInvoice } = require("./vars.helper");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-// const TerminalNumber = 1000; // Company terminal
-// const UserName = "barak9611"; // API User
-// const CreateInvoice = true; // to Create Invoice (Need permissions to create invoice )
-// const IsIframe = true; // Iframe or Redirect
-// const Operation = 4;
-
-// Create Post Information
-// Account vars
-// const vars = [];
-// vars["TerminalNumber"] = TerminalNumber;
-// vars["UserName"] = UserName;
-// vars["APILevel"] = "10"; // req
-// vars["codepage"] = "65001"; // unicode
-// vars["Operation"] = Operation;
-
-// vars["Language"] = "en"; // page languge he- hebrew , en - english , ru , ar
-// vars["CoinID"] = "1"; // billing coin , 1- NIS , 2- USD other , article :  http://kb.cardcom.co.il/article/AA-00247/0
-// vars["SumToBill"] = "79.4"; // Sum To Bill
-// vars["ProductName"] = "Test Product"; // Product Name , will how if no invoice will be created.
-
-// vars["SuccessRedirectUrl"] =
-//   "https://secure.cardcom.solutions/DealWasSuccessful.aspx"; // Success Page
-// vars["ErrorRedirectUrl"] =
-//   "https://secure.cardcom.solutions/DealWasUnSuccessful.aspx?customVar=1234"; // Error Page
-
-// Other Optional vars :
-
-// vars["CancelType"] = "2"; // show Cancel button on start ,
-// vars["CancelUrl"] ="https://payment-cardcom.herokuapp.com/";
-// //vars["IndicatorUrl"] = "http://www.yoursite.com/NotifyURL"; // Indicator Url \ Notify URL . after use -  http://kb.cardcom.co.il/article/AA-00240/0
-
-// vars["ReturnValue"] = "1234"; // Optional , ,recommended , value that will be return and save in CardCom system
-// vars["MaxNumOfPayments"] = "12"; // max num of payments to show  to the user
-
-// vars["ShowInvoiceHead"] = "true"; //  if show & edit Invoice Details on the page.
-// vars["InvoiceHeadOperation"] = "1";
+const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
- app.use('/', require('./routes').init);
-// app.use("/payment", require('./routes').payment);
+// token payment - token charge
+app.use("/charge-token", require("./routes").chargeToken);
+
+// regular payment - cc charge
+app.use("/payment", require("./routes").payment);
+
+// charge-hold-deal
+app.use("/charge-hold-deal", require("./routes").chargeHoldDeal);
+
+// cancel hold deal / refund hold deal
+app.use("/cancel-hold-deal", require("./routes").chargeHoldDeal);
+
+// refund token
+app.use("/refund-token", require("./routes").refundToken);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+//success page
+app.get("/success-page", (req, res) => {
+  res.render("success");
+});
+
+// Indicator URL
+app.get("/NotifyURL", async (req, res) => {
+  try {
+    const isDeal = await getDealIndication();
+    console.log(isDeal)
+    if (isDeal.ResponseCode == 0 && isDeal.lowprofilecode) {
+      res.json(isDeal);
+    }
+  } catch (err) {
+    console.error(err);
+    res.json(false);
+  }
+});
+
+async function getDealIndication() {
+  const url = `https://secure.cardcom.solutions/Interface/BillGoldGetLowProfileIndicator.aspx?`;
+  let config = [];
+  config["terminalnumber"] = 1000;
+  config["username"] = "barak9611";
+  config["lowprofilecode"] = "2e4346ba-e521-4d87-95cd-cf160c39dbc9";
+  const str = querystring.encode(config);
+  const response = await axios.get(url + str);
+  const obj = querystring.decode(response.data);
+  return obj;
+}
 
 app.listen(PORT, console.log("App is running on ", PORT));
